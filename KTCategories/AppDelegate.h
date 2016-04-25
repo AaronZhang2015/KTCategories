8e5b83e9b5248f93d1a012d7525ff7386b73939c
aaron zhang <aaronzhang8741@gmail.com>
2016-04-25 11:25:31 +0800

    Initial Commit

---
+++ KTCategories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTCategories
+//
+//  Created by ZhangMing on 4/25/16.
+//  Copyright © 2016 ZhangMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
